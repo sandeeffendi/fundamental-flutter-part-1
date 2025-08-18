22dc5fd7fe6009d3818c0be8cd5622783458e2de
sandeeffendi <sande.effendi@gmail.com>
2025-08-18 23:07:52 +0700

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
 import 'package:design_ui_dengan_widget/latihan_inherited_widget/inherited_widget_homepage.dart';
-import 'package:design_ui_dengan_widget/latihan_tourism_app/models/tourism.dart';
-import 'package:design_ui_dengan_widget/latihan_tourism_app/my_tourism_app.dart';
-import 'package:design_ui_dengan_widget/latihan_tourism_app/routes/navigation_routes.dart';
-import 'package:design_ui_dengan_widget/latihan_tourism_app/screens/details_screen.dart';
-import 'package:design_ui_dengan_widget/latihan_tourism_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
