28beafffd4e7012a800d52cba86050e6530e091f
sandeeffendi <sande.effendi@gmail.com>
2025-08-28 01:08:44 +0700

    done: dependency injection TourismListProvider, TourismDetailProvider, dan ApiServices

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
-import 'package:design_ui_dengan_widget/latihan_theming_typography/data/models/tourism.dart';
+import 'package:design_ui_dengan_widget/latihan_theming_typography/data/api/api_services.dart';
 import 'package:design_ui_dengan_widget/latihan_theming_typography/my_tourism_app.dart';
 import 'package:design_ui_dengan_widget/latihan_theming_typography/routes/navigation_routes.dart';
 import 'package:design_ui_dengan_widget/latihan_theming_typography/screens/detail/details_screen.dart';
 import 'package:design_ui_dengan_widget/latihan_theming_typography/screens/main/main_screen.dart';
 import 'package:design_ui_dengan_widget/provider/bookmark_list_provider.dart';
 import 'package:design_ui_dengan_widget/provider/bottom_navigation_provider.dart';
+import 'package:design_ui_dengan_widget/provider/detail/tourism_detail_provider.dart';
+import 'package:design_ui_dengan_widget/provider/home/tourism_list_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,9 +16,17 @@ void main() {
       providers: [
         ChangeNotifierProvider(create: (context) => BottomNavigationProvider()),
         ChangeNotifierProvider(create: (context) => BookmarkListProvider()),
+        Provider(create: (context) => ApiServices()),
+        ChangeNotifierProvider(
+          create: (context) => TourismListProvider(context.read<ApiServices>()),
+        ),
+        ChangeNotifierProvider(
+          create: (context) =>
+              TourismDetailProvider(context.read<ApiServices>()),
+        ),
       ],
       child: MyTourismApp(),
-  ),
+    ),
   );
 }
 
